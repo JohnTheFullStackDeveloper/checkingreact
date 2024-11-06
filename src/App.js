42c613ff234010e047ecbc27aede88ecc3185b13
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-11-06 19:43:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import {io} from 'socket.io-client'
+const socket = io("https://server-25w1.onrender.com")
 function App() {
+  socket.removeAllListeners()
+  const [a,seta] = useState(0)
+  const [b,setb] = useState(0)
+  const [c,setc] = useState(0)
+  const [d,setd] = useState(0)
+  const [e,sete] =useState("")
+  const [f,setf] =useState("")
+  const sendf = ()=>{
+    socket.emit("CheckLogin",{a:a,b:b,c:c,d:d})
+  }
+  socket.on("CheckLoginS",data=>{
+    sete(data)
+  })
+  socket.on("CheckBets",data=>{
+    setf(data)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi bro</h1>
+      <h4>{e}</h4>
+      <h4>{f}</h4>
+      <input onChange={e=>seta(e.target.value)}></input><br></br>
+      <input onChange={e=>setb(e.target.value)}></input><br></br>
+      <input onChange={e=>setc(e.target.value)}></input><br></br>
+      <input onChange={e=>setd(e.target.value)}></input><br></br>
+      <button onClick={sendf}>success</button>
     </div>
   );
 }
